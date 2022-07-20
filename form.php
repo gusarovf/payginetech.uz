22463a133380be1b14d0ad3ea32aefc3f7d36968
gusarovf <6527375@mail.ru>
2022-07-20 17:49:02 +0300

    Mv contact form to php file. Start submit handle.

---
+++ form.php
@@ -0,0 +1,191 @@
+<form
+  id="form435521223"
+  name="form435521223"
+  role="form"
+  action=""
+  method="POST"
+  data-formactiontype="2"
+  data-inputbox=".t-input-group"
+  class="t-form js-form-proccess t-form_inputs-total_5 t-form_bbonly"
+  data-success-callback="t698_onSuccess"
+>
+  <input
+    type="hidden"
+    name="formservices[]"
+    value="10b129a52a5a436def0523c1767cc063"
+    class="js-formaction-services"
+  />
+  <input
+    type="hidden"
+    name="formservices[]"
+    value="c94246dc9bbe1908fcadb2768f842277"
+    class="js-formaction-services"
+  />
+  <input
+    type="hidden"
+    name="tildaspec-formname"
+    tabindex="-1"
+    value="Contact"
+  />
+  <div
+    class="js-successbox t-form__successbox t-text t-text_md"
+    style="display: none; color: #ffffff; background-color: #00dbe5"
+  >
+    Message was sent successfully
+  </div>
+  <div class="t-form__inputsbox">
+    <div class="t-input-group t-input-group_nm" data-input-lid="1495630353482">
+      <div class="t-input-block">
+        <input
+          type="text"
+          autocomplete="name"
+          name="Name"
+          class="t-input js-tilda-rule t-input_bbonly"
+          value=""
+          placeholder="Your name"
+          data-tilda-req="1"
+          data-tilda-rule="name"
+          style="color: #ffffff; border: 1px solid #d6d6d6"
+        />
+        <div class="t-input-error"></div>
+      </div>
+    </div>
+    <div class="t-input-group t-input-group_ph" data-input-lid="1649890699323">
+      <div class="t-input-block">
+        <input
+          type="tel"
+          autocomplete="tel"
+          name="Phone"
+          class="t-input js-tilda-rule t-input_bbonly"
+          value=""
+          placeholder="Your phone number"
+          data-tilda-req="1"
+          data-tilda-rule="phone"
+          pattern="[0-9]*"
+          style="color: #ffffff; border: 1px solid #d6d6d6"
+        />
+        <div class="t-input-error"></div>
+      </div>
+    </div>
+    <div class="t-input-group t-input-group_em" data-input-lid="1495629963726">
+      <div class="t-input-block">
+        <input
+          type="text"
+          autocomplete="email"
+          name="Email"
+          class="t-input js-tilda-rule t-input_bbonly"
+          value=""
+          placeholder="Your email"
+          data-tilda-req="1"
+          data-tilda-rule="email"
+          style="color: #ffffff; border: 1px solid #d6d6d6"
+        />
+        <div class="t-input-error"></div>
+      </div>
+    </div>
+    <div class="t-input-group t-input-group_ta" data-input-lid="1649882478996">
+      <div class="t-input-block">
+        <textarea
+          name="Textarea"
+          class="t-input js-tilda-rule t-input_bbonly"
+          placeholder="Your question"
+          data-tilda-req="1"
+          style="color: #ffffff; border: 1px solid #d6d6d6; height: 102px"
+          rows="3"
+        ></textarea>
+        <div class="t-input-error"></div>
+      </div>
+    </div>
+    <div class="t-input-group t-input-group_cb" data-input-lid="1649890486567">
+      <div class="t-input-block">
+        <label
+          class="t-checkbox__control t-text t-text_xs"
+          style="color: #ffffff"
+          ><input
+            type="checkbox"
+            name="Checkbox"
+            value="yes"
+            class="t-checkbox js-tilda-rule"
+            data-tilda-req="1"
+          />
+          <div class="t-checkbox__indicator"></div>
+          I give my consent to the transfer, processing and storage of my
+          personal data under the Privacy Policy</label
+        >
+        <div class="t-input-error"></div>
+      </div>
+    </div>
+    <!--[if IE 8
+      ]><style>
+        .t-checkbox__control .t-checkbox,
+        .t-radio__control .t-radio {
+          left: 0px;
+          z-index: 1;
+          opacity: 1;
+        }
+        .t-checkbox__indicator,
+        .t-radio__indicator {
+          display: none;
+        }
+        .t-img-select__control .t-img-select {
+          position: static;
+        }
+      </style><!
+    [endif]-->
+    <div class="t-form__errorbox-middle">
+      <div
+        class="js-errorbox-all t-form__errorbox-wrapper"
+        style="display: none"
+      >
+        <div class="t-form__errorbox-text t-text t-text_md">
+          <p class="t-form__errorbox-item js-rule-error js-rule-error-all"></p>
+          <p class="t-form__errorbox-item js-rule-error js-rule-error-req"></p>
+          <p
+            class="t-form__errorbox-item js-rule-error js-rule-error-email"
+          ></p>
+          <p class="t-form__errorbox-item js-rule-error js-rule-error-name"></p>
+          <p
+            class="t-form__errorbox-item js-rule-error js-rule-error-phone"
+          ></p>
+          <p
+            class="t-form__errorbox-item js-rule-error js-rule-error-minlength"
+          ></p>
+          <p
+            class="t-form__errorbox-item js-rule-error js-rule-error-string"
+          ></p>
+        </div>
+      </div>
+    </div>
+    <div class="t-form__submit">
+      <button
+        type="submit"
+        class="t-submit"
+        style="
+          color: #ffffff;
+          background-color: #00dbe5;
+          border-radius: 12px;
+          -moz-border-radius: 12px;
+          -webkit-border-radius: 12px;
+        "
+        data-btneffects-first="btneffects-light"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+  <div class="t-form__errorbox-bottom">
+    <div class="js-errorbox-all t-form__errorbox-wrapper" style="display: none">
+      <div class="t-form__errorbox-text t-text t-text_md">
+        <p class="t-form__errorbox-item js-rule-error js-rule-error-all"></p>
+        <p class="t-form__errorbox-item js-rule-error js-rule-error-req"></p>
+        <p class="t-form__errorbox-item js-rule-error js-rule-error-email"></p>
+        <p class="t-form__errorbox-item js-rule-error js-rule-error-name"></p>
+        <p class="t-form__errorbox-item js-rule-error js-rule-error-phone"></p>
+        <p
+          class="t-form__errorbox-item js-rule-error js-rule-error-minlength"
+        ></p>
+        <p class="t-form__errorbox-item js-rule-error js-rule-error-string"></p>
+      </div>
+    </div>
+  </div>
+</form>
---
+++ handle-form.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $("#contact-form").on("submit", function (e) {
+    e.preventDefault()
+    console.log("foo")
+  })
+})
