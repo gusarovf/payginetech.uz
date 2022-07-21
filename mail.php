f5b10b0902595b2525a3849f628a4a673c0cac3c
gusarovf <6527375@mail.ru>
2022-07-21 14:15:23 +0300

    Modified mail form.

--- mail.php
+++ mail.php
@@ -8,16 +8,15 @@
         // $to = 'postmaster@payginetech.uz';
         $to = "6527375@mail.ru";
 
-        $subject = 'Заполнена форма на сайте от ' . $email;
-
-        $headers = "From: " . $name . " <" . $email . "> \r\n";
+        $subject = 'Form filled.';
         $message = "" . $name . " \r\n" . $email . " \r\n" . $phone . " \r\n" . $question . " \r\n";
-        $send_email = mail($to, $subject, $message, $headers);
+        $send_email = mail($to, $subject, $message);
 
         echo json_encode(array(
             "success" => $send_email,
             "error" => !$send_email ? error_get_last()['message'] : null,
-            "msg" => $message
+            "msg" => $message,
+            "sub" => $subject
         ));
     } ?>
 
